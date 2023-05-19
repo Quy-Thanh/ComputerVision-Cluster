4537186fe42a2f23c44c5fef1f7b0324b413c11a
To_Quy_Thanh <Toquythanh2k3@gmail.com>
2023-05-20 02:21:03 +0700

    feat: call method of KMean_Clustering, plot new image

--- clustering/main.py
+++ clustering/main.py
@@ -5,13 +5,32 @@ import numpy as np
 import cv2
 
 def main():
-	image_path = r"../data/thobaymau.jpg"
-	image = cv2.imread(image_path)
-	image_array = np.array(image)
-	image_array = image_array.reshape(-1, image_array.shape[-1])
+    image_path = r"../data/Tho2.jpeg"
+    image = cv2.imread(image_path)
+    
+    # Convert the image to RGB format if it's in BGR format
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-	KMean(5)
-
+    image_array = image.reshape(-1, 3)
+    
+    kmeans = KMean(3)
+    
+    kmeans.fit(image_array)
+    
+    labels = kmeans.predict(image_array)
+    
+    # Plot the image with the clusters colored differently.
+    clustered_image = np.zeros_like(image_array)
+    for i in range(kmeans.n_clusters):
+        mask = labels == i
+        color = kmeans.centroids[i]
+        clustered_image[mask] = color
+    
+    clustered_image = clustered_image.reshape(image.shape)
+    
+    clustered_image = clustered_image.astype(np.uint8)
+    
+    cv2.imshow("Clustered Image", clustered_image)
+    cv2.waitKey(0)
 if __name__ == '__main__':
 	main()
-
