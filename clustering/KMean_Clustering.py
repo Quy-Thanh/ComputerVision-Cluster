74a0cf534fa2adb978acf7c09b56b7c8ff1be47f
To_Quy_Thanh <Toquythanh2k3@gmail.com>
2023-05-20 02:18:09 +0700

    feat: add fit method to the KMean_Clustering
    The fit method takes a data parameter and performs the K-means clustering algorithm.

--- clustering/KMean_Clustering.py
+++ clustering/KMean_Clustering.py
@@ -4,3 +4,24 @@ class KMean():
 	def __init__(self, n_clusters):
 		self.n_clusters = n_clusters
 		self.centroids = None
+		self.max_iter = 10
+
+	def fit(self, data):
+        # Initialize the centroids randomly.
+		self.centroids = np.random.randint(0, 255, (self.n_clusters, data.shape[-1]))
+
+        # Repeat until convergence or max_iter times:
+		for i in range(self.max_iter):
+			# Assign each data point to the closest centroid.
+			distances = np.linalg.norm(data - self.centroids[:, np.newaxis], axis=2)
+			labels = np.argmin(distances, axis=0)
+
+            # Update the centroids.
+			for i in range(self.n_clusters):
+				if np.any(labels == i):
+ 					new_centroid = np.mean(data[labels == i], axis=0)
+				self.centroids[i] = new_centroid
+
+            # If the centroids have not changed, then we have converged.
+			if np.allclose(new_centroid, self.centroids):
+				break
